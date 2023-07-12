853909f43963c2e29cd7c3d54c6ee79d689ae427
Yoav Yosef <yoav.yosef1@gmail.com>
2023-07-12 14:26:09 +0200

    first code snippet in main.py works

--- myenv/src/main.py
+++ myenv/src/main.py
@@ -1,17 +1,16 @@
-from curses import pair_number
 import PyPDF2
 
 def extract_text_from_pdf(file_path):
-    with open(file_path, 'rb') as file:
+    text = ""
+    with open(file_path, "rb") as file:
         reader = PyPDF2.PdfReader(file)
-        text = ''
-        for page in range(len(reader.pages)):
-            text += reader.reader.pages[pair_number].extractText()
-        return text
+        for page in reader.pages:
+            text += page.extract_text()
+    return text
 
-# Example of usage
+# Example usage
 pdf_file_path = r"C:\Users\Yoav\OneDrive\Projects\Academic_Article_Summarizer\myenv\Corpora\Example\Profile.pdf"
 text = extract_text_from_pdf(pdf_file_path)
-
 print(text)
+
 
