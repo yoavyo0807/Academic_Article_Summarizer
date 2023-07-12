a9970433f2852f5d32f64d4fac6edc9258eca74b
Yoav Yosef <yoav.yosef1@gmail.com>
2023-07-12 13:33:40 +0200

    added curses package

--- myenv/src/main.py
+++ myenv/src/main.py
@@ -1,15 +1,16 @@
+from curses import pair_number
 import PyPDF2
 
 def extract_text_from_pdf(file_path):
     with open(file_path, 'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
+        reader = PyPDF2.PdfReader(file)
         text = ''
-        for page in range(reader.numPages):
-            text += reader.getPage(page).extractText()
+        for page in range(len(reader.pages)):
+            text += reader.reader.pages[pair_number].extractText()
         return text
 
 # Example of usage
-pdf_file_path = ''
+pdf_file_path = r"C:\Users\Yoav\OneDrive\Projects\Academic_Article_Summarizer\myenv\Corpora\Example\Profile.pdf"
 text = extract_text_from_pdf(pdf_file_path)
 
 print(text)
